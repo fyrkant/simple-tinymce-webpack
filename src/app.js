d23e81d58b318b5f274a9c7f8caea5b58ca8652b
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2017-10-16 11:39:13 +0200

    update deps and remove file loader, copy files yourself!

--- src/app.js
+++ src/app.js
@@ -8,13 +8,6 @@ import 'tinymce/plugins/autoresize'
 import 'tinymce/plugins/table'
 import 'tinymce/plugins/wordcount'
 
-// File loader that handles moving the skin files
-require.context(
-  'file-loader?name=[path][name].[ext]&context=node_modules/tinymce!tinymce/skins',
-  true,
-  /.*/
-)
-
 tinymce.init({
   selector: 'textarea',
   plugins: 'paste link autoresize table wordcount',
